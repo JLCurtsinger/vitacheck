6304094a467cd729f4d79cc28ad6cc90eb711aac
Justin <98992595+JLCurtsinger@users.noreply.github.com>
2025-05-17 14:07:14 -0700

    Update App.tsx
    added google analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-
+import { useEffect } from "react";
+import { useLocation, BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { BackToTop } from "./components/BackToTop";
 import ProtectedRoute from "./components/ProtectedRoute";
@@ -13,56 +13,43 @@ import Results from "./pages/Results";
 import Experiences from "./pages/Experiences";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsAndConditions from "./pages/TermsAndConditions";
+import { trackPageView } from "./utils/analytics";
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <AuthProvider>
-        <Toaster />
-        <Sonner />
-        <BackToTop />
-        <BrowserRouter>
+const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPageView(location.pathname + location.search);
+  }, [location]);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <AuthProvider>
+          <Toaster />
+          <Sonner />
+          <BackToTop />
           <Routes>
-            {/* Remove Login route - it's no longer needed */}
-            <Route path="/" element={
-              <ProtectedRoute>
-                <Index />
-              </ProtectedRoute>
-            } />
-            <Route path="/check" element={
-              <ProtectedRoute>
-                <Check />
-              </ProtectedRoute>
-            } />
-            <Route path="/results" element={
-              <ProtectedRoute>
-                <Results />
-              </ProtectedRoute>
-            } />
-            <Route path="/experiences" element={
-              <ProtectedRoute>
-                <Experiences />
-              </ProtectedRoute>
-            } />
-            <Route path="/privacy-policy" element={
-              <ProtectedRoute>
-                <PrivacyPolicy />
-              </ProtectedRoute>
-            } />
-            <Route path="/terms" element={
-              <ProtectedRoute>
-                <TermsAndConditions />
-              </ProtectedRoute>
-            } />
-            {/* Redirect any unknown routes to home */}
+            <Route path="/" element={<ProtectedRoute><Index /></ProtectedRoute>} />
+            <Route path="/check" element={<ProtectedRoute><Check /></ProtectedRoute>} />
+            <Route path="/results" element={<ProtectedRoute><Results /></ProtectedRoute>} />
+            <Route path="/experiences" element={<ProtectedRoute><Experiences /></ProtectedRoute>} />
+            <Route path="/privacy-policy" element={<ProtectedRoute><PrivacyPolicy /></ProtectedRoute>} />
+            <Route path="/terms" element={<ProtectedRoute><TermsAndConditions /></ProtectedRoute>} />
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
-        </BrowserRouter>
-      </AuthProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
+        </AuthProvider>
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+};
+
+const AppWithRouter = () => (
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
-export default App;
+export default AppWithRouter;
